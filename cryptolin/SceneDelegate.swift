e4a1779ea60fb6e231bd0f31c8f333867cd577e0
AbimbolaOO <abimbolaolayemiwhyte@gmail.com>
2021-10-14 15:36:07 +0100

    feat: complete the onboarding implementation

--- cryptolin/SceneDelegate.swift
+++ cryptolin/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = BaseTabBarController() // Place your root view controller here
-//            window.rootViewController = Utils.createViewInNavbar(viewController: OnBoardingViewController())
+//            window.rootViewController = BaseTabBarController() // Place your root view controller here
+            window.rootViewController = Utils.createViewInNavbar(viewController: OnBoardingViewController())
             self.window = window
             window.makeKeyAndVisible()
         }
